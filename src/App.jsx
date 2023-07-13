9bfa9819e2942262b40e413bcbfe972d267410d1
kylinholmes <874067181lsfy@gmail.com>
2023-07-14 02:21:48 +0800

    add more key

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function SideBar({ show = false, set_sidebar }) {
 
   const [sub_page, set_sub_page] = useState(["常规", "科学计算器", "程序员"]);
@@ -11,7 +12,7 @@ function SideBar({ show = false, set_sidebar }) {
     <div className='flex'>
       {
         show ? <>
-          <div className='flex flex-auto flex-col gap-y-2 bg-sidebar h-300'>
+          <div className='flex flex-auto flex-col gap-y-2 bg-sidebar '>
             {
               sub_page.map((item, idx) =>
                 <button className={active_index === idx ? "selected_item" : "item"} onClick={() => set_active_index(idx)} key={idx} >
@@ -33,83 +34,104 @@ function SideBar({ show = false, set_sidebar }) {
 function Calc({ set_sidebar }) {
   const [input_buffer, set_input_buffer] = useState('0');
   return (
-    <div className='h-full w-full flex flex-col gap-10 p-4' style={ {backgroundColor: "#202020"} }>
+    <div className='h-full w-full flex flex-col gap-10 p-4' style={{ backgroundColor: "#202020" }}>
       <div className='flex flex-row'>
         <button onClick={() => set_sidebar((current) => !current)}>按钮</button>
-        <div>常规</div>
+        {/* <div>常规</div> */}
       </div>
       <Display input_buffer={input_buffer} />
-      <KeyBoard set_input_buffer={set_input_buffer}/>
+      <KeyBoard set_input_buffer={set_input_buffer} />
     </div>
   )
 }
 
-function Display({input_buffer}) {
+function Display({ input_buffer }) {
+  const select_bg = {
+    '::selection': { // 选中文本的样式
+      backgroundColor: 'yellow',
+    },
+  };
   return (
     <div className='h-20 w-full'>
-      <div className='w-full h-full text-right	text-4xl' style={ {backgroundColor: "#202020"}} >{input_buffer}</div>
+      <div id='display' className='w-full h-full flex items-center justify-end	text-5xl font-medium p-2' >
+        {input_buffer}
+      </div>
     </div>
   )
 }
 
 
-function KeyBoard({set_input_buffer}) {
+function KeyBoard({ set_input_buffer }) {
 
-  const print_event = (event) => {
+  const calcValue = (buffer) => {
+    return '0'
+  }
+
+
+  const handleEvent = (event) => {
     const value = event.target.id;
-    switch (value) {
-      case 'A/C': {
-        set_input_buffer('0')
-        break
-      }
-      case '=' : {
-
-        break
-      }
-      default : {
-        set_input_buffer((buffer) => {
-          if( buffer != '0' ) {
+    set_input_buffer((buffer) => {
+      switch (value) {
+        case '': {
+          return buffer
+        }
+        case 'A/C': {
+          return '0'
+        }
+        case '=': {
+          return calcValue(buffer);
+        }
+        case 'del': {
+          if (buffer.length == 1) {
+            return '0'
+          }
+          return buffer.slice(0, -1);
+        }
+        default: {
+          if (buffer != '0') {
             return buffer + value
           }
           return value
-        })
+        }
       }
+    });
 
-    }
-    
-    
   }
 
   return (
-    <div className="grid grid-rows-5 grid-cols-4 grid-flow-col gap-1 text-2xl h-120 " onClick={ print_event }>
+    <div className="grid grid-rows-6 grid-cols-4 grid-flow-col gap-1 text-2xl h-120 " onClick={handleEvent}>
       <Key symbol="A/C" />
+      <Key symbol="" />
       <Key symbol="7" />
       <Key symbol="4" />
       <Key symbol="1" />
+      <Key symbol="%" />
+      <Key symbol="(" />
       <Key symbol="" />
-      <Key symbol="/" />
       <Key symbol="8" />
       <Key symbol="5" />
-      {/* <div className='bg-slate-200 text-slate-900 text-center h-15 row-span-2'> + </div> */}
       <Key symbol="2" />
       <Key symbol="0" />
-      <Key symbol="*" />
+      <Key symbol=")" />
+      <Key symbol="" />
       <Key symbol="9" />
       <Key symbol="6" />
       <Key symbol="3" />
       <Key symbol="." />
+      <Key symbol="del" />
+      <Key symbol="/" />
       <Key symbol="-" />
-      <Key className='row-span-2 ' symbol="+" />
-      {/* <div class="">.</div> */}
-      <Key className='row-span-2 ' symbol='=' />
+      <Key symbol="*" />
+      <Key symbol="+" />
+      <Key extraClass='bg-keyblue hover:bg-keybluehover active:bg-keyblueactive text-black' symbol='=' />
 
     </div>
   )
 }
 
-function Key({ symbol, className }) {
+function Key({ symbol, extraClass = 'bg-key hover:bg-keyhover active:bg-keyactive text-white' }) {
   return (
-    <div id={symbol} className={className + ' flex items-center bg-key text-white justify-center hover:bg-keyhover	'} >
+    <div id={symbol} className={' flex items-center justify-center rounded-md font-light  ' + extraClass} >
       {symbol}
     </div>
   )
@@ -118,7 +140,7 @@ function Key({ symbol, className }) {
 function App() {
   const [show_sidebar, set_sidebar] = useState(false);
   return (
-    <div className='relative  w-150 h-300'>
+    <div className='relative  w-full h-full min-w-120 '>
       <div className='w-full  left-0 top-0 absolute'>
         <SideBar show={show_sidebar} set_sidebar={set_sidebar} />
       </div>
--- uno.config.ts
+++ uno.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
       'bgdark': '#202020',
       'key': '#323232',
       'keyhover': '#3c3c3c',
+      'keyactive': '#282828',
+      'keyblue': "#80d2ff",
+      'keybluehover': "#76c0e8",
+      'keyblueactive': "#6caed2",
       'sidebar': "#2c2c2c"
     }
   }
